9d643f601f979387693c73a5cdfa8c3991994a08
Darshit-parzival <darshitrakhasia@gmail.com>
2026-02-21 11:00:17 +0530

    project set up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>FleetFlow</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @include('layouts.css')
 </head>
+
 <body class="bg-light">
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="#">FleetFlow</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">FleetFlow</a>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        @yield('content')
     </div>
-</nav>
-
-<div class="container mt-5">
-    @yield('content')
-</div>
-
+    @include('layouts.js')
 </body>
+
 </html>
